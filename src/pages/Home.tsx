1c0af925343688e834f212200dbc2c14c3dc9685
sin-n581 <sacans581@gmail.com>
2026-07-08 22:14:39 +0200

    Navbar and uncompleted Hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,24 @@
+import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
-
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return <div className ="min-h-screen bg-background text-foreground overflow-x-hidden"> 
   
     {/* Theme toggle*/}
      <ThemeToggle />
+
     {/* Backround Effects*/}
       <StarBackground />
+
     {/* Navbar*/}
+      <Navbar />
 
     {/* Main Content */}
-
+      <main>
+        <HeroSection />
+      </main>
 
     {/* Footer */}
 
