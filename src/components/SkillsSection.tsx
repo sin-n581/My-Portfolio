5e1d7b993dab8d5b92215f5791fe10ab0b111557
sin-n581 <sacans581@gmail.com>
2026-07-09 23:55:09 +0200

    SkillsSection first draft

---
+++ src/components/SkillsSection.tsx
@@ -0,0 +1,33 @@
+
+const skills = [
+    { name: "JavaScript", level:95, category: "language" },
+    { name: "C#", level:90, category: "language" },
+    { name: "TypeScript", level:85, category: "language" },
+    { name: "SQL", level:80, category: "language" },
+    { name: "HTML/CSS", level:95, category: "language" },
+    
+    { name: "React", level:90, category: "framework" },
+    { name: "Express", level:85, category: "framework" },
+    { name: "Tailwind CSS", level:90, category: "framework" },
+    { name: "Next.js", level:80, category: "framework" },
+    { name: "Node.js", level:80, category: "framework" },
+
+    { name: "Git/Github", level:90, category: "tools" },
+    { name: "Figma", level:70, category: "tools" },
+    { name: "Vite", level:85, category: "tools" },
+    { name: "Heroku", level:85, category: "tools" },
+    
+    ];
+
+
+
+
+
+export const SkillsSection = () => {
+    return <section
+     id="skills" 
+     className="py-24 px relative bg-secondary/30"
+     >
+
+     </section>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
   return <div className ="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -20,6 +21,7 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
       </main>
 
     {/* Footer */}
