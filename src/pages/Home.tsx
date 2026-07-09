461fe409110428cb81c8ef817a0d80cccbfe487a
sin-n581 <sacans581@gmail.com>
2026-07-09 19:28:51 +0200

    About me, with placeholder text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = () => {
   return <div className ="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -18,6 +19,7 @@ export const Home = () => {
     {/* Main Content */}
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
 
     {/* Footer */}
