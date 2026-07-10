f1679f879687236620dff1e4b9298520572b36b3
sin-n581 <sacans581@gmail.com>
2026-07-10 23:38:39 +0200

    Get-in-touch-Section Complete

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -29,9 +29,11 @@ export const HeroSection = () => {
         </div>
     
 
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <a href="#about" className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        
             <span className="rext-sm text-muted-foreground mb-2">Scroll</span>
             <ArrowDown className="h-5 w-5 text-primary" />
-        </div>
+            
+        </a>
     </section>
 }
