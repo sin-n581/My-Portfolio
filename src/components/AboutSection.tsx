f55238a8277df14fffd5790b8fa9329164d0ab0d
sin-n581 <sacans581@gmail.com>
2026-07-19 23:55:04 +0200

    CV downloading option added

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -34,8 +34,8 @@ export const AboutSection = () => {
                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                         <a href="#contact" className="cosmic-button">Get In Touch</a>
 
-                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transit"> 
-                        Download CV
+                        <a href="/Lebenselauf-Sin.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transit"> 
+                        Lebenslauf
                         </a>
 
                     </div>
@@ -70,7 +70,7 @@ export const AboutSection = () => {
                                 <h4 className="font-semibold text-lg">UI/UX Design</h4>
                                 <p className="text-muted-foreground">
                                     Entwicklung einer intuitiven Benutzeroberfläche
-                                    und eines nahtlosen Benutyzrerlebnisses.
+                                    und eines nahtlosen Benutzererlebnisses.
                                 </p>
                             </div>
                         </div>
